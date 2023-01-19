39017c6b859cf029900f8fec3e897728635ce1cc
Adarsh <adarsh800251@gmail.com>
2023-01-19 22:59:00 +0530

    added url

--- backEnd/server.js
+++ backEnd/server.js
@@ -37,7 +37,7 @@ const server = app.listen(PORT,console.log(`server is listening to port ${PORT}`
 const io  = require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000"
+        origin:"https://chat-app-frontend-rust.vercel.app"
     },
 })
 
